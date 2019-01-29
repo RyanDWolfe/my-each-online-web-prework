168cd48c8503c2f35f037db9e28ebdc45b9cccc7
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-29 20:12:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
 def my_each(array)
-  i = array.size
-  counter = 0
-  while counter < i
-    array
-    counter += 1
+  i = 0
+  while i < array.length
+     yield array[i]
+    i += 1
   end
   array
 end
